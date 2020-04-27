d61129b010fc42a1ebe94d334240ca93ff87df28
pablossanz <40574657+pablossanz@users.noreply.github.com>
2020-04-27 04:11:03 +0200

    menu buttons update & cursor style over button

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,29 @@
 function hidemenu() {
      window.location.href = "inicio.html";
 }
+
+function Usuario() {
+     window.location.href = "inicio.html";
+}
+
+function vehiculos() {
+     window.location.href = "inicio.html";
+}
+
+function pagos() {
+     window.location.href = "inicio.html";
+}
+
+function reservas() {
+     window.location.href = "inicio.html";
+}
+
+function logout() {
+     window.location.href = "start.html";
+    delete_cookie("mail");
+    //delete rest of cookies
+}
+
+function delete_cookie( name ) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
