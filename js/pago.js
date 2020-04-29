fefb00d3b8c99a9ba9d731f7ffc8eff3b00b6e17
pablossanz <40574657+pablossanz@users.noreply.github.com>
2020-04-29 21:02:50 +0200

    registro pago funcional

--- js/pago.js
+++ js/pago.js
@@ -11,3 +11,40 @@ function continue_button() {
 
     window.location.href = "inicio.html"
 }
+
+
+function tarjeta() {
+    if((document.getElementById('datos-box').style.visibility)=='visible') {
+        document.getElementById('datos-box').style.visibility='hidden';
+        document.getElementById('button-tarjeta').style.background='#e5a227';
+    }
+    else {
+        document.getElementById('datos-box').style.visibility='visible'
+        document.getElementById;
+        document.getElementById('button-tarjeta').style.background='#CA8D13';
+        document.getElementById('button-paypal').style.background='#e5a227';
+    } 
+    if((document.getElementById('paypal-box').style.visibility)=='visible') {
+        document.getElementById('paypal-box').style.visibility='hidden';
+        document.getElementById('button-paypal').style.background='#e5a227';
+    }
+}
+
+function paypal() {
+    if((document.getElementById('datos-box').style.visibility)=='visible') {
+        document.getElementById('datos-box').style.visibility='hidden';
+        document.getElementById('button-tarjeta').style.background='#e5a227';
+    }
+    if((document.getElementById('paypal-box').style.visibility)=='visible') {
+        document.getElementById('paypal-box').style.visibility='hidden';
+        document.getElementById('button-paypal').style.background='#e5a227';
+    }
+    else {
+        document.getElementById('paypal-box').style.visibility='visible';
+        document.getElementById('button-paypal').style.background='#CA8D13';
+    } 
+}
+
+function gopaypal_button() {
+    window.location.href = "inicio.html"
+}
