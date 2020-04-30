a2a5d0ee713df02a6304f9eb723ba347da5b92b9
Alejandro Bartolome Fernandez <alejandro.bartolomef@alumnos.upm.es>
2020-05-01 00:20:35 +0200

    Dynamic list *happy echo noises*

--- js/vh_list.js
+++ js/vh_list.js
@@ -1,32 +1,54 @@
-
 var vehicles = [
     {matricula: '0000AAA', 
-    features : [wheelchair = 1, truck = 1, baby_carriage = 1, charging_station = 1]},
-    {matricula: '1234ABC', features : [wheelchair = 0, truck = 0, baby_carriage = 0, charging_station = 1]},
-    {matricula: '3124KSD', features : [wheelchair = 0, truck = 0, baby_carriage = 1, charging_station = 0]}
+    features : [wheelchair = true, truck = true, baby_carriage = true, charging_station = true]},
+    {matricula: '1234ABC', features : [wheelchair = false, truck = false, baby_carriage = false, charging_station = true]},
+    {matricula: '3124KSD', features : [wheelchair = false, truck = false, baby_carriage = true, charging_station = false]}
 ];
-function addVehicle() {
+
+function setVehicles(){
+    for(var i = 0; i<vehicles.length; i++){
+        addVehicle(vehicles[i].matricula, vehicles[i].features[0],  vehicles[i].features[1], vehicles[i].features[2], vehicles[i].features[3], "card"+i);
+    }
+}
+
+function addVehicle(matricula, w,t,b,e,id) {
     const div = document.createElement('div');
       
-    div.className = 'menu_button';
-      
+    div.className = 'menu-button';
+
+    div.id = id;
+
+    var a ="";
+    var tr ="";
+    var c ="";
+    var d ="";
+
+    if(w){
+        var d = `<i class="icon fas fa-wheelchair feature_icons"></i>`;
+    }
+    if(t){
+        var tr = `<i class="icon fas fa-truck feature_icons"></i>`;
+    }
+    if(b){
+        var a = `<i class="icon fas fa-baby-carriage feature_icons"></i>`;
+    }
+    if(e){
+        var c = `<i class="icon fas fa-charging-station feature_icons"></i>`;
+    }
+   
     div.innerHTML = `
-    <h1>0000AAA</h1>
+    <h1>`+matricula+`</h1>
     <i class="icon fas fa-car"></i>
-    <div class="feature_icon_box">
-        <i class="icon fas fa-wheelchair feature_icons"></i>
-        <i class="icon fas fa-truck feature_icons"></i>
+    <div class="feature_icon_box">`+a+tr+c+d+`
     </div>
     <i class="fas fa-trash thrash_icon"></i>
     `;
-    
-    var c = document.querySelector('lista');
-    c.appendChild(div);
-    //document.getElementById('lista').appendChild(div);
+      
+    document.getElementById('lista').appendChild(div);
 }
       
-addVehicle();
 
-function removeRow(input) {
-    document.getElementById('content').removeChild(input.parentNode);
+
+function removeVehicle(input) {
+    document.getElementById('lista').removeChild(input.parentNode);
 }
