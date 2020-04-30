f27ccc442e758cdb8ad9cee179ab60d337a5394e
Alejandro Bartolome Fernandez <alejandro.bartolomef@alumnos.upm.es>
2020-04-30 14:12:19 +0200

    Vehicles first attempt

--- js/inicio.js
+++ js/inicio.js
@@ -8,27 +8,10 @@ function Search_destination() {
 
 function getUsername() {
     var name = sessionStorage.getItem('name')
-    if(name === " "){
-        name = "Username";
+    if(name == ""){
+        name = "admin";
     }
     return name;
-}
-
-function getCookie(cname) {
-    var value = " ";
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for(var i = 0; i<ca.length; i++){
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            value = c.substring(name.length, c.length);
-        }
-    }
-    return value;
 }
 
 function displaymenu() {
