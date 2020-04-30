e5a92d55178e0ad031f924776e4839b10add879d
OscarAlvarezRomo <oscar.alvarez.romo@alumnos.upm.es>
2020-04-30 16:45:47 +0200

    modificación en los nombres de inicio y start a principal e inicio respectivamente

---
+++ js/principal.js
@@ -0,0 +1,36 @@
+function Search_destination() {
+    var destination = document.getElementById("destination").value;
+
+    sessionStorage.setItem('destination' , destination);
+
+    window.location.href = "buscador.html";
+}
+
+function getUsername() {
+    var name = sessionStorage.getItem('name')
+    if(name == null){
+        name = "admin";
+    }
+    return name;
+}
+
+function getCookie(cname) {
+    var value = " ";
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for(var i = 0; i<ca.length; i++){
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            value = c.substring(name.length, c.length);
+        }
+    }
+    return value;
+}
+
+function displaymenu() {
+    window.location.href = "menu.html";
+}
