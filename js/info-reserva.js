5b0bdef700fbdabf1d37f35159fe0b41f2fe7776
Alejandro Bartolome Fernandez <alejandro.bartolomef@alumnos.upm.es>
2020-05-01 18:31:29 +0200

    cozaz

--- js/info-reserva.js
+++ js/info-reserva.js
@@ -1,9 +1,5 @@
 function aceptar_button(){
-	if (Vrepostar == true) {
-		window.location.href = "repostar.html"
-	}
-	else if (Vlavado == true) {
-		window.location.href = "lavado.html"
-	}
-	else window.location.href = "metodo_pago.html"
+
+	window.location.href = "menu.html"
+
 }
--- js/tarjeta_list.js
+++ js/tarjeta_list.js
@@ -5,7 +5,7 @@ function update(){
     if(sessionStorage.getItem('paypal') != null){
         pagos.push(sessionStorage.getItem('paypal'));
         sessionStorage.removeItem('paypal');
-    }else{
+    }else if (sessionStorage.getItem('tarjeta') != null){
         var tarjeta = sessionStorage.getItem('tarjeta');
         pagos.push(tarjeta);
         sessionStorage.removeItem('tarjeta');
