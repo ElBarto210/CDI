effd5acc0f4747aa9f4c073a840c80063bd40e5b
pablossanz <40574657+pablossanz@users.noreply.github.com>
2020-05-01 20:39:58 +0200

    error registro

--- js/register_pd.js
+++ js/register_pd.js
@@ -6,25 +6,9 @@ function continue_button() {
     var pass = document.getElementById('pass').value;
     var pass2 = document.getElementById('pass2').value;
     
-
-    if(pass == pass2) {
-
-        var name = sessionStorage.setItem('name',name );
-        var last_name = sessionStorage.setItem('last_name',last_name );
-        var phone = sessionStorage.setItem('phone',phone );
-        var mail = sessionStorage.setItem('mail',mail );
-        var pass = sessionStorage.setItem('pass',pass );
-        
-        window.location.href = "register_vh.html";
-    } 
-    else {
-        window.alert("Las contraseñas no son iguales");
-    }
-
     if(mail == 'ejemplo@hotmail.com') {
         window.alert("El email introducido ya está en uso");
     }
-
     else {
         
         if(pass == pass2) {
@@ -34,7 +18,8 @@ function continue_button() {
             sessionStorage.setItem("mail", mail);
             sessionStorage.setItem("pass", pass);
             window.location.href = "register_vh.html";
+        } else {
+        window.alert("Las contraseñas no son iguales");
         } 
-        
     }
 }
